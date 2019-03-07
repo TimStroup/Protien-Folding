1b7b8740e6367dabb3f1ec104266d83f558007a6
tstroup@rams.colostate.edu <tstroup@rams.colostate.edu>
2019-03-06 21:17:43 -0700

    pushing to github now

--- src/com/company/ProteinFolder.java
+++ src/com/company/ProteinFolder.java
@@ -1,8 +1,23 @@
 package com.company;
 
-public class ProteinFolder {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class ProteinFolder {
+    private static ArrayList<String> sequence = new ArrayList<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        readRNASequence("input.txt");
+        System.out.println(Arrays.toString(sequence.toArray()));
     }
+
+    private static void readRNASequence(String fileName) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(fileName));
+        String[] letters = scan.next().split("");
+        sequence.addAll(Arrays.asList(letters));
+    }
+
 }
